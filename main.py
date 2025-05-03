ca542ecdec9535a9e3b2123a281c4f8d6beaf140
kkarbowy <k.karbowy2401@gmail.com>
2025-05-03 12:06:13 +0200

    Aktualizacja

--- main.py
+++ main.py
@@ -3,20 +3,3 @@ class Product:
         self.sku = sku
         self.name = name
         self.price = price
-
-class ShoppingCart:
-    def __init__(self):
-        self.items = {}
-
-    def add_product(self, product, quantity=1):
-        self.items[product] = self.items.get(product, 0) + quantity
-
-    def remove_product(self, product, quantity=1):
-        if product in self.items:
-            new_q = self.items[product] - quantity
-            if new_q > 0:
-                self.items[product] = new_q
-            else:
-                del self.items[product]
-        else:
-            print(f"Produkt {product.name} nie jest w koszyku.")
